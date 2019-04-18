76d2034176163e341524638ba92f1552c0ecd996
RDX <xrd0312@outlook.com>
2019-04-18 14:14:14 +0800

    190418 h

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# file: __init__.py
-# author: songyouwei <youwei0314@gmail.com>
-# Copyright (C) 2018. All Rights Reserved.
-
-from models.atae_lstm import ATAE_LSTM
-from models.aen import CrossEntropyLoss_LSR, AEN, AEN_BERT
-from models.aoa import AOA
-from models.tt import TargetedTransformer
